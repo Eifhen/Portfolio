1e8b8a1dc58e1e974ead9a454624e57e9fc4cc6a
Gabriel <Jimenezg905@gmail.com>
2023-10-03 22:07:43 -0400

    fix: correccion onRotueChange

--- src/hooks/onRouteChange.hook.tsx
+++ src/hooks/onRouteChange.hook.tsx
@@ -3,12 +3,13 @@ import { createEffect } from "solid-js";
 
 
 /*
-  Ejecuta un callback cada vez que la ruta cambia
+  Ejecuta un callback cada vez que la ruta o 
+  el hash de la ruta cambia
 */
 export default function onRouteChange(callback: (path:string) => void) {
   const location = useLocation();
   createEffect(()=>{
-    if(location.pathname){
+    if(location.hash || location.pathname){
       callback(location.pathname);
     }
   })
