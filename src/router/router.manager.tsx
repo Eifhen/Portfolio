c177932feb3288554d4970260957166118c0a701
Gabriel <jimenezg905@gmail.com>
2024-02-21 18:03:12 -0400

    fix: redirect to home

--- src/router/router.manager.tsx
+++ src/router/router.manager.tsx
@@ -2,7 +2,6 @@ import { Navigate, Outlet, Route, Router, Routes } from '@solidjs/router';
 import TestPage from '../pages/test/test.page';
 import 'remixicon/fonts/remixicon.css';
 import '../assets/css/general.css';
-import { AplicationContextProvider } from '../context/aplication.context';
 import Layout from '../components/layout/layout.component';
 import HomePage from '../pages/home/home.page';
 import ProjectPage from '../pages/projects/projects.page';
@@ -12,15 +11,14 @@ import AboutPage from '../pages/about/about.page';
 import TemplatesPage from '../pages/templates/templates.page';
 import TemplateDetailPage from '../pages/templates_detail/templates_detail';
 import LogoPage from '../pages/test/logo.page';
-import { onRouteChange } from '../hooks/scrollSmooth';
-import { onMount } from 'solid-js';
+
 
 export default function RouterManager(){
     return(
         <Router>
             <Routes>
                 <Route path="" component={ Layout }>
-                    <Route path="/" component={ HomePage }/>
+                    <Route path="/" element={<Navigate href="home" />}/>
                     <Route path="/home" component={ HomePage }/>
                     <Route path="/projects" component={ ProjectPage } />
                     <Route path="/projects/detail/:id/:project-name" component={ ProjectDetailPage } />                    
